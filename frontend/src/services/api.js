a30ec38f377c921cb6d3833326bb6bc10bfdf5ea
2303A52161 <2303A52161@sru.edu.in>
2026-06-13 09:29:20 +0530

    repushing all

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-rpaj.onrender.com/api/products",
+  baseURL: "http://localhost:3000",
 });
 
 export default API;
